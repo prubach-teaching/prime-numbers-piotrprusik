e8ec7bc601db2abb6b96f4304ff2c3a248dbcd7a
piotrprusik <61974584+piotrprusik@users.noreply.github.com>
2020-03-28 15:24:25 +0100

    Update PrimeNumbers.java

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -2,24 +2,36 @@ package sgh;
 
 public class PrimeNumbers {
 
-    public static void primes(int n) {
-        
-        
-                for(int i=2;i<=n;i++)
+    public static void primes(int n)
+    {
+
+        int count = 0;
+
+                for (int i = 2; i <= 100000; i++)
                 {
-                    for(int j=2;j<=i;j++)
+
+                    for (int j = 2; j <= i; j++)
                     {
-                        if (j==i)
+
+                        if (j == i)
                         {
-                            System.out.print(i+", ");
+                            System.out.print(i + ", ");
+                            count=count+1;
                         }
-                        if ( i % j == 0)
+                        if (i % j == 0)
                         {
                             break;
                         }
+
+                    }
+                    if (count == n)
+                    {
+                        break;
                     }
                 }
     }
+
+
     
   public static void main(String[] args) {
         primes(4);
