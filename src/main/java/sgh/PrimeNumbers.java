523d6eeaf9bab34ebee22739aca1b65303891ff7
piotrprusik <61974584+piotrprusik@users.noreply.github.com>
2020-03-28 14:00:56 +0100

    Update PrimeNumbers.java

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -5,8 +5,7 @@ public class PrimeNumbers {
     public static void primes(int n) {
         public static void main(String[] args)
     {
-        int n = 10;
-
+        
                 for(int i=2;i<=n;i++)
                 {
                     for(int j=2;j<=i;j++)
